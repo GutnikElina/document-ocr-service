33f52ad1939d7f47d0046e39c800fb2bd4c4924a
Alexander Dudkin <raddanprofile@gmail.com>
2026-08-18 16:31:15 +0300

    Initial work

---
+++ .github/pull_request_template.md
@@ -0,0 +1,21 @@
+Something...
+
+### Changes:
+
+* Something...
+
+---
+
+### Progress
+
+- [ ] Change must be properly reviewed (3 review required)
+- [x] Change must not contain extraneous whitespace
+- [x] Commit message must refer to an issue
+
+### Issue
+
+- [ID](URL): Name
+
+### Reviewers
+
+- [Name](GitHub_URL)
---
+++ src/main/java/com/innowise/logistics/ocr/DocumentOcrServiceApplication.java
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2026 Aliaksandr Dudkin
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.innowise.logistics.ocr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentOcrServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentOcrServiceApplication.class, args);
+    }
+}
